17df44c2ef2420db4ed6b7ae654754b624506cba
Martin Alexandersson <martin.alexandersson@sspa.se>
2020-11-04 18:53:14 +0100

    ggg

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="hooks", # Replace with your own username
-    version="0.0.12",
+    version="0.0.13",
     author="Example Author",
     author_email="author@example.com",
     description="A small example package",
